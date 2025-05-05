e939c271b6e2d4f2a8534e3fb70c3bbf079f9e1b
Sonavin <sonavin.rem@gmail.com>
2025-05-06 04:58:24 +0700

    Another commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Tools\\SDK\\flutter-sdk" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\my_first_flutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Tools\\SDK\\flutter-sdk"
+  "PROJECT_DIR=E:\\my_first_flutter"
+  "FLUTTER_ROOT=D:\\Tools\\SDK\\flutter-sdk"
+  "FLUTTER_EPHEMERAL_DIR=E:\\my_first_flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\my_first_flutter"
+  "FLUTTER_TARGET=E:\\my_first_flutter\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\my_first_flutter\\.dart_tool\\package_config.json"
+)
